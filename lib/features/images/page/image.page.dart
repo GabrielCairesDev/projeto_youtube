87c49c61e65d3bf7591defcbd92e3dc2d5dee827
Gabriel Caires <gabriel.araujo.caires@gmail.com>
2026-01-26 10:52:00 -0300

    feat: criado pagina para renderizar imagens. Local e remoto

--- lib/features/images/page/image.page.dart
+++ lib/features/images/page/image.page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:projeto_youtube/shared/widgets/image_cache.widget.dart';
 import 'package:projeto_youtube/shared/widgets/scaffold.widget.dart';
 
 class ImagePage extends StatefulWidget {
@@ -11,9 +13,28 @@ class ImagePage extends StatefulWidget {
 class _ImagePageState extends State<ImagePage> {
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldWidget(
+    return ScaffoldWidget(
       title: 'Image Page',
-      body: Center(child: Text('This is the Image Page')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blueGrey,
+              child: Image.asset('assets/images/imagem_local.jpg'),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.redAccent,
+              child: ImageCacheWidget(
+                imageUrl:
+                    'https://wallpapers.com/images/hd/clouds-4k-rdw4c7q5mddwp60l.jpg',
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
     );
   }
 }
---
+++ lib/shared/widgets/image_cache.widget.dart
@@ -0,0 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ImageCacheWidget extends StatelessWidget {
+  const ImageCacheWidget({super.key, required this.imageUrl});
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      placeholder: (context, url) => _Loading(),
+      errorWidget: (context, url, error) => _Erro(),
+    );
+  }
+}
+
+class _Loading extends StatelessWidget {
+  const _Loading();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: CircularProgressIndicator());
+  }
+}
+
+class _Erro extends StatelessWidget {
+  const _Erro();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: Text('Failed to load image'),
+      ),
+    );
+  }
+}
